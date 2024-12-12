ade0e0589be6bc0f551d020a675915d2afa569ec
SaimonC <155955381+saimoncollaku@users.noreply.github.com>
2024-12-12 15:25:46 +0100

    ex 3 started

---
+++ ex03_harris/matlab_code/selectKeypoints.m
@@ -0,0 +1,8 @@
+function keypoints = selectKeypoints(scores, num, r)
+% Selects the num best scores as keypoints and performs non-maximum 
+% supression of a (2r + 1)*(2r + 1) box around the current maximum.
+
+% TODO: Your code here
+
+end
+
---
+++ ex03_harris/matlab_code/shi_tomasi.m
@@ -0,0 +1,24 @@
+function scores = shi_tomasi(img, patch_size)
+
+% TODO: Your code here
+f_sum = (patch_size ^ 2) * fspecial("average",[patch_size, patch_size]);
+I_x = conv2(img, fspecial("sobel")', 'valid');
+I_y = conv2(img, fspecial("sobel"), 'valid'); 
+
+I_x2 = I_x.^2;
+I_y2 = I_y.^2;
+I_xy = I_x .* I_y;
+
+E_x2 = conv2(I_x2, f_sum, "valid");
+E_y2 = conv2(I_y2, f_sum, "valid");
+E_xy = conv2(I_xy, f_sum, "valid");
+
+lambda_1 = 0.5 .* (E_x2 + E_y2 + sqrt(4 .* E_xy .* E_xy + (E_x2 - E_y2).^2));
+lambda_2 = 0.5 .* (E_x2 + E_y2 - sqrt(4 .* E_xy .* E_xy + (E_x2 - E_y2).^2));
+
+R = min(lambda_1, lambda_2);
+R = max(R, 0);
+scores = padarray(R, ones(2,1) * floor(patch_size / 2), 0);
+
+
+end
