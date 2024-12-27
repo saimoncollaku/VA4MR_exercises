e9c593ecdf68fb57d15dac732bb534b1ab54bf5d
SaimonC <155955381+saimoncollaku@users.noreply.github.com>
2024-12-27 14:02:01 +0100

    Ex 8 started

---
+++ ex08_ba/matlab_code/runBA.m
@@ -0,0 +1,7 @@
+function hidden_state = runBA(hidden_state, observations, K)
+% Update the hidden state, encoded as explained in the problem statement,
+% with 20 bundle adjustment iterations.
+% TODO: Your code here
+end
+
+ 
