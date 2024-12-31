21a6da87e8338370e82d44223d14ccb48dd0793d
SaimonC <155955381+saimoncollaku@users.noreply.github.com>
2024-12-31 15:35:29 +0100

    ex 9 part 2

--- ex09_klt/matlab_code/getWarpedPatch.m
+++ ex09_klt/matlab_code/getWarpedPatch.m
@@ -2,8 +2,23 @@ function patch = getWarpedPatch(I, W, x_T, r_T)
 % x_T is 1x2 and contains [x_T y_T] as defined in the statement. patch is
 % (2*r_T+1)x(2*r_T+1) and arranged consistently with the input image I.
 
+patch = zeros(2 * r_T + 1);
 
+for idx = 1 : numel(patch)
+    [y, x] = ind2sub(size(patch), idx);
+    orig_coord = [x, y, 1];
+    warp_coord = x_T + (orig_coord - r_T - 1) * W';
 
+    if warp_coord(1) < 1 || warp_coord(2) < 1
+        continue;
+    end
 
+    if warp_coord(1) > width(I) || warp_coord(2) > height(I)
+        continue;
+    end
+
+    patch(y, x) = I(warp_coord(2), warp_coord(1));
+end
 
 end
+
--- ex09_klt/matlab_code/trackBruteForce.m
+++ ex09_klt/matlab_code/trackBruteForce.m
@@ -4,6 +4,29 @@ function [dx, ssds] = trackBruteForce(I_R, I, x_T, r_T, r_D)
 % of patch to search dx within; dx: translation that best explains where
 % x_T is in image I, ssds: SSDs for all values of dx within the patch
 % defined by center x_T and radius r_D.
-% TODO: Your code here
 
+range = -r_D : r_D;
+numElements = numel(range);
+W_0 = getSimWarp(0, 0, 0, 1);
+T = getWarpedPatch(I_R, W_0, x_T, r_T);
+ssds = zeros(2 * r_D + 1);
+
+for idx = 1 : ((2 * r_D +1)^2)
+    % Obtaining dx and dy
+    [row, col] = ind2sub([numElements, numElements], idx);
+    dx = range(col);
+    dy = range(row);
+    
+    % Warping query with dx
+    W = getSimWarp(dx, dy, 0, 1);
+    I_w = getWarpedPatch(I, W, x_T, r_T);
+
+    % Computing and storing error
+    ssd = sum((I_w - T).^2, "all");
+    ssds(dx + r_D + 1, dy + r_D + 1) = ssd;
 end
+
+[~ , idx] = min(ssds);
+[dx, dy] = ind2sub(size(ssds), idx);
+dx = [dx, dy] - r_D - 1;
+
